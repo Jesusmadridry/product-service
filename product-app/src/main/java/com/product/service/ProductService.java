750caecd36d71bb0bf070a2dba01a511bdc2503d
Jesus Madrid <114009973+JesusMadridMCH@users.noreply.github.com>
2024-10-04 18:46:23 -0500

    task_01-Product-Service: Defining method names

--- product-app/src/main/java/com/product/service/ProductService.java
+++ product-app/src/main/java/com/product/service/ProductService.java
@@ -6,11 +6,21 @@ import com.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
     public Mono<ProductResponse> registerNewProduct(ProductView productView){
+        return Mono.empty();
+    }
+
+    public Mono<ProductResponse> modifyProduct(ProductView productView){
+        return Mono.empty();
+    }
+
+    public Mono<ProductResponse> deleteProduct(UUID internalProductId){
         return Mono.empty();
     }
 }
