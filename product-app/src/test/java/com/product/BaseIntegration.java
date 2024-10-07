31e1a98ef97b29b1c846e41c3c7a742b3c57a7d9
Jesus De Los Reyes Madrid <79116789+Jesusmadridry@users.noreply.github.com>
2024-10-07 15:43:45 -0500

    Task 05 create unit testing for product service (#4)
    * task-05-create-unit-testing for-product-service : Setting up base integration class for unit tests
    
    * ttask-05-create-unit-testing for-product-service: Created first test for create product method. There was included the successful case.
    
    * task-05-create-unit-testing for-product-service: Created use case when there was tried to create an already existing product. Created a use case to get the wrong response when there was a null payload in the new register product method
    
    *task-05-create-unit-testing for-product-service : Defining success modifying product test.
    
    * task-05-create-unit-testing for-product-service: Defining failure case when it was tried to be modified a non-existing record associated to the code received
    
    * task-05-create-unit-testing for-product-service: Defining success case when a product will be deleted.
    
    * task-05-create-unit-testing for-product-service Fixing logging level in test cases
    
    ---------
    
    Co-authored-by: Jesus Madrid <114009973+JesusMadridMCH@users.noreply.github.com>

---
+++ product-app/src/test/java/com/product/BaseIntegration.java
@@ -0,0 +1,52 @@
+package com.product;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.product.mapper.ProductMapper;
+import com.product.repository.CategoryRepository;
+import com.product.repository.ProductRepository;
+import com.product.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.reset;
+
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@SpringBootTest(classes = ProductServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureWebTestClient
+public class BaseIntegration {
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @SpyBean
+    protected ProductMapper productMapper;
+
+    @SpyBean
+    protected ProductService productService;
+
+    @SpyBean
+    protected ProductRepository productRepository;
+
+    @SpyBean
+    protected CategoryRepository categoryRepository;
+
+
+    @BeforeEach
+    void setUp(){
+        productRepository.deleteAll();
+        categoryRepository.deleteAll();
+        reset(productService);
+        reset(productRepository);
+        reset(categoryRepository);
+    }
+
+}
