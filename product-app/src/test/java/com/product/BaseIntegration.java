937a4e4b68947f07f1472053bd8849e1ef879927
Jesus De Los Reyes Madrid <jesusmadridry@gmail.com>
2024-10-07 18:25:22 -0500

    task_07-Test-for-Controller-Payload: removed delete all categories

--- product-app/src/test/java/com/product/BaseIntegration.java
+++ product-app/src/test/java/com/product/BaseIntegration.java
@@ -5,6 +5,7 @@ import com.product.mapper.ProductMapper;
 import com.product.repository.CategoryRepository;
 import com.product.repository.ProductRepository;
 import com.product.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +17,11 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Scanner;
+
 import static org.mockito.Mockito.reset;
 
 @ExtendWith({MockitoExtension.class, SpringExtension.class})
@@ -23,6 +29,7 @@ import static org.mockito.Mockito.reset;
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureWebTestClient
+@Slf4j
 public class BaseIntegration {
     @Autowired
     protected ObjectMapper objectMapper;
@@ -43,10 +50,19 @@ public class BaseIntegration {
     @BeforeEach
     void setUp(){
         productRepository.deleteAll();
-        categoryRepository.deleteAll();
         reset(productService);
         reset(productRepository);
-        reset(categoryRepository);
+    }
+
+    protected String jsonContent(String name) {
+        String srcPath = String.format("samples/%s.json", name);
+        log.info("Loading response from {}", srcPath);
+
+        InputStream src = getClass().getClassLoader().getResourceAsStream(srcPath);
+        Objects.requireNonNull(src, "Unable to find " + srcPath);
+
+        Scanner scanner = new Scanner(new InputStreamReader(src)).useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
     }
 
 }
