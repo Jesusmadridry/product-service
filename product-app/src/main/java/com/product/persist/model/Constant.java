30aa53f86e476ee377e6991f93e5d779e9ddf757
Jesus De Los Reyes Madrid <jesusmadridry@gmail.com>
2024-10-07 17:06:03 -0500

    task_06-Spring-Security: Creating roles for every permission

--- product-app/src/main/java/com/product/persist/model/Constant.java
+++ product-app/src/main/java/com/product/persist/model/Constant.java
@@ -6,8 +6,8 @@ public final class Constant {
     public static final String AUDIT_SCHEMA_GENERATOR = "audit_generator";
 
 
-    public static final String ERROR_MESSAGE_SERVICE = "There was an error trying to create the product";
-    public static final String MANDATORY_FIELD = "Product Flow is mandatory to process the request";
+    public static final String ERROR_MESSAGE_SERVICE = "There was an error trying to process the product";
+    public static final String MANDATORY_FIELD = "Product Payload is mandatory to process the request";
 
     public static final String EXISTING_PRODUCT_MESSAGE = "This product is already created";
     public static final String NEW_PRODUCT_MESSAGE = "This product was created successfully";
